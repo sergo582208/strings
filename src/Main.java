cf1526e290c10d9ae9d8d0877cc366f7f28c6aff
hp <747685@mail.ru>
2023-02-20 21:39:33 +0300

    строки дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+    }
+
+    public static void task1() {
+        String firstName = "Ivan";
+        String middleName = firstName + " Ivanovich";
+        String lastName = middleName + " Ivanov";
+        String fullName = lastName;
+        System.out.println("ФИО сотрудника " + " — " + fullName);
+
+
+    }
+
+    public static void task2() {
+        String firstName = "Ivan";
+        firstName = firstName.replace("Ivan", "IVAN");
+        String middleName = firstName + " Ivanovich";
+        middleName = middleName.replace("Ivanovich", "IVANOVICH");
+        String lastName = middleName + " Ivanov";
+        lastName = lastName.replace("Ivanov", "IVANOV");
+        String fullName = lastName;
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + lastName);
+    }
+
+    public static void task3() {
+        String firstName = "Семён";
+        firstName = firstName.replace("Семён", "Семен");
+        String middleName = firstName + " Семёнович";
+        middleName = middleName.replace("Семёнович", "Семенович");
+        String lastName = middleName + " Иванов";
+        lastName = lastName.replace("Иванов", "Иванов");
+        String fullName = lastName;
+        System.out.println("Данные ФИО сотрудника  — " + lastName);
     }
 }
